50497ea94a3369f0b2797ffea9cb2be57175ee26
Manolet Balanay <manolet@proudcloud.net>
2011-12-20 15:06:31 +0800

    Test for changes

--- lib/golia.rb
+++ lib/golia.rb
@@ -103,8 +103,7 @@ class Golia
       puts "  http://validator.lipsiasoft.com/check?uri=#{link}"
     end
     puts "Average load time %0.2fsec" % [@sec.inject(0) { |memo, sec| memo+=sec; memo }/@sec.size]
-    @avg_load_time = @sec.inject(0) { |memo, sec| memo+=sec; memo }/@sec.size
-
+    puts "This is my edit #{@avg_load_time = @sec.inject(0) { |memo, sec| memo+=sec; memo }/@sec.size}"
     return @avg_load_time
   end
 end
